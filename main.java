b40300931d0d1f9ef72f57135583f5ffbd732126
BenKir <benjamin.kirschner@gmail.com>
2023-09-20 10:42:03 +0200

    my first commit

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bitte geben Sie eine Nummer ein!");
+
+        scanner.close();
+
+    }
+}
